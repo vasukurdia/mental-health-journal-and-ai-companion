3a4382f506fcb8c2faa89aacbe0f38d9296b1361
Vasu Kurdia <vasukurdia@gmail.com>
2026-08-13 14:23:16 +0530

    Updated code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
         </div>
       </nav>
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-14">
         <div className="text-center">
           <h2 className="text-5xl font-display font-bold mb-6">
             <span className="text-gradient">Your Mental Wellness</span>
